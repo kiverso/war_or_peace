a85f542d78f8c743d6aa4ecfe37b9021d839dea2
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-03-21 23:35:19 -0600

    Add war_or_peace_runner.rb

---
+++ war_or_peace_runner.rb
@@ -0,0 +1,26 @@
+require './lib/card.rb'
+require './lib/deck.rb'
+require './lib/player.rb'
+require './lib/turn.rb'
+
+
+standard_values = [*(2..10), 'Jack', 'Queen', 'King', 'Ace']
+standard_suits = ['Clubs', 'Hearts', 'Diamonds', 'Spades']
+all_cards = []
+
+standard_suits.each do|suit|
+  standard_values.each do |value|
+    all_cards << Card.new(suit, value, standard_values.index(value) + 2)
+  end
+end
+
+full_deck = Deck.new(all_cards)
+shuffled_deck = Deck.new(full_deck.cards.shuffle)
+deck1 = Deck.new([])
+deck2 = Deck.new([])
+
+while shuffled_deck.cards.length > 0 do
+  deck1.add_card(shuffled_deck.remove_card)
+  deck2.add_card(shuffled_deck.remove_card)
+end
+require "pry"; binding.pry
